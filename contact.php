372002ce920b1b334674dd4136a35a2497bc5255
Adam Soltys <asoltys@gmail.com>
2009-07-02 18:41:41 -0700

    Added Vancouver office to contact page

--- contact.php
+++ contact.php
@@ -3,19 +3,32 @@
 <h1>Contact Us</h1>
 
 <div class="page_section">
-	<p>
-		Dynamic Investigations Inc. <br/>
+	<h2>
+		Dynamic Investigations Inc.
+  </h2>
+
+  <p>
+    <strong>Head Office</strong><br />
 		505-8840-210th Street  Suite#: 160<br/>
 		Langley, B.C.<br/>
-		V1M 2Y2<br/>
-		<br/>
+		V1M 2Y2
+  </p>
+
+  <p>
+    <strong>Vancouver Office</strong><br />
+    #53558-984 West Broadway<br />
+    Vancouver, B.C.<br />
+    V5Z 1K0
+  </p>
+
+
+  <p>	
 		604-888-8893 (Office)<br/>
-		604-721-7535 (Cel)<br/>
-		604-888-5893 (fax)<br/>
+		604-721-7535 (Cell)<br/>
+		604-888-5893 (Fax)<br/>
 		1-866-700-8893 (Toll Free)<br />
-		<br />
 		Email: <a href="mailto:ron@dynamicinvestigations.com">ron@dynamicinvestigations.com</a>
-	</p>
+  </p>
 	
 </div>
 
