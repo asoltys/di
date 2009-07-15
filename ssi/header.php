a7dce64d7a0e2b3e268b153bc3ce8d064d733b24
Adam Soltys <asoltys@gmail.com>
2009-07-14 20:40:44 -0700

    Updated meta content

--- ssi/header.php
+++ ssi/header.php
@@ -2,10 +2,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>Dynamic Investigations</title>
+  <title>Dynamic Investigations ¿ Vancouver Private Investigator</title>
 
-  <meta http-equiv="description" content="Dynamic Investigations Inc. is an insured, bonded and licensed company in accordance to the Private Investigators’ and Securities Act pursuant to the Solicitor General and Public Safety Ministry of British Columbia.">
-  <meta http-equiv="keywords" content="Dynamic Investigations, Private Investigator, Vancouver, Langley, British Columbia">
+  <meta http-equiv="description" content="Get the results that you need!  Your investigative source in B.C.">
+  <meta http-equiv="keywords" content="private investigator vancouver, Private Investigator BC, Private Investigator British Columbia, Private Investigators Vancouver, Private Investigator in Vancouver, Private Investigator lower mainland, Private investigator fraser valley, fraud in british columbia, private investigator surrey, employee fraud bc, private investigator vancouver bc, private investigator in bc, private investigation vancouver, private investigation bc, surveillance,pi in vancouver, pi in bc, pi in british columbia">
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 
   <link href="css/main.css" type="text/css" media="all" rel="stylesheet" />
