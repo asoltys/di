5720efad00fa55b3b44e0cc7104e5c5d31711893
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-11-21 10:28:45 -0700

    removing icbc testimonials

--- index.php
+++ index.php
@@ -22,20 +22,10 @@
 	
 	<div>
 		<p>
-			<q>The best video that I have ever seen</q>
-			<cite>-ICBC Adjuster</cite>
-		</p>
-		
-		<p>
 			<q>Thank you so much.  Your video evidence assisted me in destroying the credibility of the other party.</q>
 			<cite>-Dreyer & Company Law Offices</cite>
 		</p>
-		
-		<p>
-			<q>We were very successful at trial thanks to you.</q>
-			<cite>-ICBC Defense Counsel</cite>
-		</p>
-		
+
 		<p>
 			<q>The second best video I have ever seen; however, the best video that I have seen, the company filmed the wrong person.</q>
 			<cite>-Litigation Support</cite>
